8a4b5d5407c70ab2af678f9a9447af912c1659b0
TruongThiMaiHue <82595922+TruongThiMaiHue@users.noreply.github.com>
2021-10-20 01:11:11 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express'); //đi vào thư mục node modules tải nạp ra ngoài lưu vào biến
+const app = express();//gọi function, function này đã được viết sẵn và trả lại một đối tượng, đại diện cho ứng dụng của các bạn.
+const port = 3000;
+app.get('/', (req, res) => res.send('Hello world!')) //arrow function
+// định nghĩa cái route
+/*c1: declaration function
+app.get('/', function(req,res){ 
+    return res.send('Hello world!')
+})
+c2: full arrow function
+app.get('/', (req,res) => { 
+    return res.send('Hello world!')
+})*/
+app.listen(port, () => console.log(`Server is running at port ${port}`))
